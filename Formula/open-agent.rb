a62c245ddd3678fc28558329e842458fd504daa9
imhassla <imhassla@users.noreply.github.com>
2026-08-16 10:01:32 +0000

    open-agent 0.4.0

--- Formula/open-agent.rb
+++ Formula/open-agent.rb
@@ -1,20 +1,20 @@
 class OpenAgent < Formula
   desc "Cost-ladder agentic CLI over OpenRouter"
   homepage "https://github.com/imhassla/open-agent"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/imhassla/open-agent/releases/download/v0.3.0/open-agent_0.3.0_darwin_arm64.tar.gz"
-      sha256 "5a8f3dc49ee1e3d5dd9b69c91f5737f4970236be6e6b38b5b3ee444902d4db7e"
+      url "https://github.com/imhassla/open-agent/releases/download/v0.4.0/open-agent_0.4.0_darwin_arm64.tar.gz"
+      sha256 "da2d575bd83399447e0432536f7d002108f4fb66140d162c0ac9bfaa8b78fd95"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/imhassla/open-agent/releases/download/v0.3.0/open-agent_0.3.0_linux_amd64.tar.gz"
-      sha256 "870ca25050d8b7f33f862001e7d46fb8d594f48fb0d123988efa7936a79924b9"
+      url "https://github.com/imhassla/open-agent/releases/download/v0.4.0/open-agent_0.4.0_linux_amd64.tar.gz"
+      sha256 "66525090543fe349872d122e5ed0697e54c673ecf9e53ae167d653effa1d9b0a"
     end
   end
 
